36e5c21e2f6e549a5c6926493bcc9ed50a2bcf32
Andrey Smirnov <andreysm1996@mail.ru>
2020-11-30 23:34:25 +0300

    Fix code review remarks

--- spring-homework-1/src/main/java/ru/otus/spring/service/QuestionOutputService.java
+++ spring-homework-1/src/main/java/ru/otus/spring/service/QuestionOutputService.java
@@ -3,6 +3,8 @@ package ru.otus.spring.service;
 import lombok.AllArgsConstructor;
 import ru.otus.spring.dao.QuestionDao;
 
+import java.util.Map;
+
 @AllArgsConstructor
 public class QuestionOutputService {
     private final QuestionDao questionDao;
@@ -10,12 +12,12 @@ public class QuestionOutputService {
     void showQuestions() {
         questionDao.getQuestionList().forEach(question -> {
             System.out.println(question.getQuestion());
-            final int[] i = {1};
 
-            question.getAnswers().forEach((s, aBoolean) -> {
-                System.out.print(i[0] + ")" + s + "  ");
-                i[0]++;
-            });
+            int i = 1;
+            for (Map.Entry<String, Boolean> answer : question.getAnswers().entrySet()) {
+               System.out.print(i + ")" + answer.getKey() + "  ");
+                i++;
+            }
             System.out.println();
         });
     }
