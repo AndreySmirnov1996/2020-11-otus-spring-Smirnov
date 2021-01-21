22d9599c98cfb31e1b5e8b446caf458443aa9139
Andrey Smirnov <andreysm1996@mail.ru>
2021-01-21 23:48:16 +0300

    hw5 refactoring test

--- spring-homework-5/src/test/java/ru/otus/spring/repositories/BookRepositoryImplTest.java
+++ spring-homework-5/src/test/java/ru/otus/spring/repositories/BookRepositoryImplTest.java
@@ -6,14 +6,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import ru.otus.spring.domain.Author;
 import ru.otus.spring.domain.Book;
 import ru.otus.spring.domain.Genre;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DisplayName("Репозиторий на основе Jdbc для работы со студентами ")
 @JdbcTest
@@ -25,14 +28,44 @@ class BookRepositoryImplTest {
 
     @DisplayName("должен находить книгу по id")
     @Test
-    void findById() {
-        Genre genre = new Genre(99, "new_genre_name");
-        Author author = new Author(89, "author_name", "author_surname");
-        Book book = new Book(512, "book_title", genre, new ArrayList<>());
-        book.getAuthors().add(author);
+    void findByIdTest() {
+        val bookId = 111;
+        Author author1 = new Author(11, "author_name_1", "author_surname_1");
+        Author author2 = new Author(22, "author_name_2", "author_surname_2");
+        Book book = createBook(bookId, "book_title", new Genre(11, "genre_1"),
+                Arrays.asList(author1, author2));
+
+        val bookOpt = bookRepository.findById(bookId);
+        assertThat(bookOpt).isNotEmpty().get().isEqualTo(book);
+    }
+
+    @DisplayName("должен сохранять книгу")
+    @Test
+    void saveTest() {
+        Book book = createBook(512, "book_title_new", new Genre(99, "new_genre_name"),
+                Collections.singletonList(new Author(89, "author_name", "author_surname")));
         bookRepository.save(book);
 
         val bookOpt = bookRepository.findById(book.getId());
         assertThat(bookOpt).isNotEmpty().get().isEqualTo(book);
+    }
+
+    @DisplayName("должен находить все книги со всей информацией")
+    @Test
+    void findAllWithAllInfoTest() {
+        val expectedBooks = 3;
+        List<Book> books = bookRepository.findAllWithAllInfo();
+        assertEquals(expectedBooks, books.size());
+        books.forEach(book -> {
+                    assertNotNull(book.getTitle());
+                    assertNotNull(book.getGenre());
+                    assertNotNull(book.getAuthors());
+                }
+        );
+    }
+
+
+    private Book createBook(long bookId, String bookTitle, Genre genre, List<Author> authors) {
+        return new Book(bookId, bookTitle, genre, authors);
     }
 }
