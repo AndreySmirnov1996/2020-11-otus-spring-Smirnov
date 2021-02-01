faabead0c0e215d9acd4ea8a68dd49bcba36e139
Andrey Smirnov <andreysm1996@mail.ru>
2021-02-01 23:49:37 +0300

    hw6 Fix comments test

--- spring-homework-6/src/test/java/ru/otus/spring/repositories/CommentRepositoryImplTest1.java
+++ spring-homework-6/src/test/java/ru/otus/spring/repositories/CommentRepositoryImplTest1.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.context.annotation.Import;
 import ru.otus.spring.domain.Book;
 import ru.otus.spring.domain.Comment;
@@ -12,14 +13,15 @@ import ru.otus.spring.domain.Comment;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Репозиторий на основе JPA для работы с комментариями должен ")
 @DataJpaTest
 @Import(CommentRepositoryImpl.class)
 class CommentRepositoryImplTest1 {
 
+    @Autowired
+    private TestEntityManager em;
     @Autowired
     private CommentRepositoryImpl commentRepository;
 
@@ -28,39 +30,40 @@ class CommentRepositoryImplTest1 {
     void findByIdTest() {
         val commentId = 7;
 
-        Comment commentExp = Comment.builder()
+        Comment expectedComment = Comment.builder()
                 .text("Good book!")
                 .book(Book.builder().id(111).build())
                 .build();
 
-        val commentOpt = commentRepository.findById(commentId);
-        assertThat(commentOpt).isNotEmpty();
+        val actualCommentOptional = commentRepository.findById(commentId);
+        assertThat(actualCommentOptional).isNotEmpty();
 
-        val commentAct = commentOpt.get();
-        assertEquals(commentId, commentAct.getId());
-        assertEquals(commentExp.getText(), commentAct.getText());
-        assertEquals(commentExp.getBook().getId(), commentAct.getBook().getId());
+        val actualComment = actualCommentOptional.get();
+        assertEquals(commentId, actualComment.getId());
+        assertEquals(expectedComment.getText(), actualComment.getText());
+        assertEquals(expectedComment.getBook().getId(), actualComment.getBook().getId());
 
     }
 
     @DisplayName("должен сохранять комментарий")
     @Test
     void saveTest() {
-        Comment comment = createComment("Some bad comment", Book.builder().id(111).build());
-        commentRepository.save(comment);
+        Comment expectedComment = createComment("Some bad comment", Book.builder().id(111).build());
+        commentRepository.save(expectedComment);
 
-        val bookOpt = commentRepository.findById(comment.getId());
-        assertThat(bookOpt).isNotEmpty().get().isEqualTo(comment);
+        val actualComment = em.find(Comment.class, expectedComment.getId());
+        assertNotNull(actualComment);
+        assertThat(actualComment).isEqualTo(expectedComment);
     }
 
     @DisplayName("должен находить все комментарии по id книги")
     @Test
     void findAllTest() {
-        val expectedComments = 1;
+        val expectedCommentsSize = 1;
         val bookId = 111;
         List<Comment> comments = commentRepository.findAllByBookId(bookId);
 
-        assertEquals(expectedComments, comments.size());
+        assertEquals(expectedCommentsSize, comments.size());
         comments.forEach(comment -> {
                     assertNotNull(comment.getText());
                     assertNotNull(comment.getBook());
@@ -71,10 +74,10 @@ class CommentRepositoryImplTest1 {
     @DisplayName("должен удалять комментарий по id")
     @Test
     void deleteTest() {
-        val commentId = 7;
+        val commentId = 7L;
         commentRepository.delete(commentId);
-        val commentOpt = commentRepository.findById(commentId);
-        assertThat(commentOpt).isEmpty();
+        val actualComment = em.find(Comment.class, commentId);
+        assertNull(actualComment);
     }
 
     private Comment createComment(String text, Book book) {
