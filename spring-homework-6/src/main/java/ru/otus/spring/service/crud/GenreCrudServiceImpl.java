3acd1048fb7f3754d082ff27ec40fb9df6263852
Andrey Smirnov <andreysm1996@mail.ru>
2021-02-02 23:28:07 +0300

    hw6 add crud genre service

---
+++ spring-homework-6/src/main/java/ru/otus/spring/service/crud/GenreCrudServiceImpl.java
@@ -0,0 +1,29 @@
+package ru.otus.spring.service.crud;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.otus.spring.domain.Genre;
+import ru.otus.spring.repositories.GenreRepository;
+import ru.otus.spring.service.IOService;
+import ru.otus.spring.service.OutputFormatter;
+
+@Service
+@RequiredArgsConstructor
+public class GenreCrudServiceImpl implements GenreCrudService {
+
+    private final IOService ioService;
+    private final OutputFormatter outputFormatter;
+    private final GenreRepository genreRepository;
+
+    @Override
+    public void saveGenre(String name) {
+        genreRepository.save(Genre.builder()
+                .name(name)
+                .build());
+    }
+
+    @Override
+    public void showAllGenres() {
+        genreRepository.findAll().forEach(genre -> ioService.printString(outputFormatter.formatGenre(genre)));
+    }
+}
