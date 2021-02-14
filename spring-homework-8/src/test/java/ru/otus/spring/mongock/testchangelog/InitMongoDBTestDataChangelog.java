6ea6291c8d3fbe06a274b6c434b9493e5d868d5b
Andrey Smirnov <andreysm1996@mail.ru>
2021-02-14 18:29:39 +0300

    hw8 fix tests for books

---
+++ spring-homework-8/src/test/java/ru/otus/spring/mongock/testchangelog/InitMongoDBTestDataChangelog.java
@@ -0,0 +1,49 @@
+package ru.otus.spring.mongock.testchangelog;
+
+import com.github.cloudyrock.mongock.ChangeLog;
+import com.github.cloudyrock.mongock.ChangeSet;
+import com.mongodb.client.MongoDatabase;
+import ru.otus.spring.domain.Author;
+import ru.otus.spring.domain.Book;
+import ru.otus.spring.domain.Comment;
+import ru.otus.spring.domain.Genre;
+import ru.otus.spring.repositories.AuthorRepository;
+import ru.otus.spring.repositories.BookRepository;
+import ru.otus.spring.repositories.CommentRepository;
+import ru.otus.spring.repositories.GenreRepository;
+
+import java.util.Collections;
+import java.util.List;
+
+@ChangeLog(order = "001")
+public class InitMongoDBTestDataChangelog {
+
+    private Genre tragedyGenre;
+    private Author williamShakespeareAuthor;
+    private Book romeoAndJulietBook;
+
+    @ChangeSet(order = "000", id = "dropDb", author = "assmirnov", runAlways = true)
+    public void dropDb(MongoDatabase db) {
+        db.drop();
+    }
+
+    @ChangeSet(order = "001", id = "initGenres", author = "assmirnov", runAlways = true)
+    public void initGenres(GenreRepository repository) {
+        tragedyGenre = repository.save(new Genre("1", "Tragedy"));
+    }
+
+    @ChangeSet(order = "002", id = "initAuthors", author = "assmirnov", runAlways = true)
+    public void initAuthors(AuthorRepository repository) {
+        williamShakespeareAuthor = repository.save(new Author("111", "William", "Shakespeare"));
+    }
+
+    @ChangeSet(order = "003", id = "initBooks", author = "assmirnov", runAlways = true)
+    public void initBooks(BookRepository repository) {
+        romeoAndJulietBook = repository.save(new Book("1234", "Romeo and Juliet", tragedyGenre, Collections.singletonList(williamShakespeareAuthor)));
+    }
+
+    @ChangeSet(order = "004", id = "initComments", author = "assmirnov", runAlways = true)
+    public void initComments(CommentRepository repository) {
+        repository.save(new Comment("The best tragedy!", romeoAndJulietBook));
+    }
+}
--- spring-homework-8/src/test/java/ru/otus/spring/repositories/BookRepositoryImplTest.java
+++ spring-homework-8/src/test/java/ru/otus/spring/repositories/BookRepositoryImplTest.java
@@ -4,68 +4,32 @@ import lombok.val;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.context.annotation.Import;
+import ru.otus.spring.AbstractRepositoryTest;
 import ru.otus.spring.domain.Author;
 import ru.otus.spring.domain.Book;
 import ru.otus.spring.domain.Genre;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Репозиторий для работы с книгами должен ")
-@DataJpaTest
-@Import({CommentRepository.class, GenreRepository.class, AuthorRepository.class, BookRepository.class})
-class BookRepositoryImplTest {
+class BookRepositoryImplTest extends AbstractRepositoryTest {
 
-    @Autowired
-    private TestEntityManager em;
     @Autowired
     private BookRepository bookRepository;
+    @Autowired
+    private GenreRepository genreRepository;
 
-    @DisplayName("должен находить книгу по id")
-    @Test
-    void findByIdTest() {
-        val bookId = 111L;
-
-        Author author1 = new Author("11", "author_name_1", "author_surname_1");
-        Author author2 = new Author("22", "author_name_2", "author_surname_2");
-        Book book = createBook("book_name_1", new Genre("11", "genre_1"),
-                Arrays.asList(author1, author2));
-
-        val actualBook = em.find(Book.class, bookId);
-        assertNotNull(actualBook);
-
-        assertEquals(book.getTitle(), actualBook.getTitle());
-        assertEquals(book.getGenre().getName(), actualBook.getGenre().getName());
-        assertEquals(book.getAuthors().size(), actualBook.getAuthors().size());
-        assertAuthor(book.getAuthors().get(0), author1);
-        assertAuthor(book.getAuthors().get(1), author2);
-    }
-
-    @DisplayName("должен сохранять книгу")
-    @Test
-    void saveTest() {
-        Book book = createBook("book_title_new", new Genre("new_genre_name"),
-                Collections.singletonList(Author.builder().name("author_name").surname("author_surname").build()));
-        bookRepository.save(book);
-
-        val actualBook = em.find(Book.class, book.getId());
-        assertThat(actualBook).isNotNull().isEqualTo(book);
-    }
-
-    @DisplayName("должен находить все книги со всей информацией")
+    @DisplayName(" находить все книги со всей информацией")
     @Test
     void findAllWithAllInfoTest() {
-        val expectedBooks = 1;
+        val expectedBooksSize = 1;
         List<Book> books = bookRepository.findAll();
-        assertEquals(expectedBooks, books.size());
+        books.forEach(System.out::println);
+        assertEquals(expectedBooksSize, books.size());
         books.forEach(book -> {
                     assertNotNull(book.getTitle());
                     assertNotNull(book.getGenre());
@@ -74,15 +38,59 @@ class BookRepositoryImplTest {
         );
     }
 
-
-    private void assertAuthor(Author authorExp, Author authorAct) {
-        assertEquals(authorExp.getId(), authorAct.getId());
-        assertEquals(authorExp.getName(), authorAct.getName());
-        assertEquals(authorExp.getSurname(), authorAct.getSurname());
+    @DisplayName(" находить книгу по id")
+    @Test
+    void findByIdTest() {
+        val bookId = "1234";
+        val actualBook = bookRepository.findById(bookId).orElse(null);
+        assertNotNull(actualBook);
+        assertEquals(bookId, actualBook.getId());
+        assertEquals("Romeo and Juliet", actualBook.getTitle());
+        assertEquals("1", actualBook.getGenre().getId());
+        assertEquals("Tragedy", actualBook.getGenre().getName());
+        assertEquals(1, actualBook.getAuthors().size());
+        assertEquals("William", actualBook.getAuthors().get(0).getName());
+        assertEquals("Shakespeare", actualBook.getAuthors().get(0).getSurname());
     }
 
-    private Book createBook(String bookTitle, Genre genre, List<Author> authors) {
+    @DisplayName(" сохранять книгу")
+    @Test
+    void saveTest() {
+        val bookId = "1112";
+        Book book = saveAndReturnBook(bookId);
+        val actualBook = bookRepository.findById(book.getId());
+        assertThat(actualBook).isPresent().get().isEqualTo(book);
+        bookRepository.deleteById(bookId);
+    }
+
+    @DisplayName(" удалять книгу по id")
+    @Test
+    void deleteByIdTest() {
+        val bookId = "1112";
+        saveAndReturnBook(bookId);
+
+        val beforeDeleteBook = bookRepository.findById(bookId).orElse(null);
+        assertNotNull(beforeDeleteBook);
+        bookRepository.deleteById(bookId);
+        val afterDeleteBook = bookRepository.findById(bookId).orElse(null);
+        assertNull(afterDeleteBook);
+    }
+
+
+    private Book saveAndReturnBook(String bookId) {
+        Author author1 = new Author("11", "author_name_1", "author_surname_1");
+        Author author2 = new Author("22", "author_name_2", "author_surname_2");
+        Genre genre = new Genre("11", "genre_1");
+        Book book = createBook(bookId, "book_name_1", new Genre("11", "genre_1"),
+                Arrays.asList(author1, author2));
+        genreRepository.save(genre);
+        bookRepository.save(book);
+        return book;
+    }
+
+    private Book createBook(String id, String bookTitle, Genre genre, List<Author> authors) {
         return Book.builder()
+                .id(id)
                 .title(bookTitle)
                 .genre(genre)
                 .authors(authors)
