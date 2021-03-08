2c7644bcdbe13f34c6de497f42e3a3fba5db00b7
Andrey Smirnov <andreysm1996@mail.ru>
2021-03-08 23:07:14 +0300

    hw9 parse fix

---
+++ spring-homework-9/src/test/java/ru/otus/spring/WebApplicationTests.java
@@ -0,0 +1,18 @@
+package ru.otus.spring;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@DisplayName("Веб приложение библиотека должно")
+@SpringBootTest
+@AutoConfigureMockMvc
+class WebApplicationTests {
+
+    @DisplayName("корректно запускать контекст")
+    @Test
+    void contextLoads() {
+    }
+
+}
