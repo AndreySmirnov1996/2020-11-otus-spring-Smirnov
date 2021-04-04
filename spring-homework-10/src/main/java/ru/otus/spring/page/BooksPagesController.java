258d6a06d00c36e549a82f63cdc4c2e1e8365f7d
Andrey Smirnov <andreysm1996@mail.ru>
2021-04-04 17:47:51 +0300

    hw10 fix base methods

--- spring-homework-10/src/main/java/ru/otus/spring/page/BooksPagesController.java
+++ spring-homework-10/src/main/java/ru/otus/spring/page/BooksPagesController.java
@@ -1,20 +1,44 @@
 package ru.otus.spring.page;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import ru.otus.spring.domain.Book;
+import ru.otus.spring.rest.dto.BookDto;
+import ru.otus.spring.service.crud.BookCrudService;
 
 @Controller
+@RequiredArgsConstructor
+@Slf4j
 public class BooksPagesController {
+
+    private final BookCrudService bookCrudService;
 
     @GetMapping("/")
     public String indexPage() {
         return "index";
     }
 
+    @PostMapping("/book/delete")
+    public String deleteBookOld(@RequestParam long id) {
+        bookCrudService.deleteBookById(id);
+        return "redirect:/";
+    }
 
     // Форма для редактирования книги
     @GetMapping("/book/{id}/edit")
-    public String editBook() {
+    public String editBook(@PathVariable("id") long id, Model model) throws ChangeSetPersister.NotFoundException {
+        Book book = bookCrudService.findById(id).orElseThrow(ChangeSetPersister.NotFoundException::new);
+        model.addAttribute("book", BookDto.toDto(book));
         return "edit_book";
+    }
+
+    @PostMapping(value = "/book/edit")
+    public String editBook(@ModelAttribute("book") BookDto book) {
+        bookCrudService.updateBookTitleById(book.getId(), book.getTitle());
+        return "redirect:/";
     }
 }
--- spring-homework-10/src/main/java/ru/otus/spring/rest/BookController.java
+++ spring-homework-10/src/main/java/ru/otus/spring/rest/BookController.java
@@ -21,33 +21,13 @@ public class BookController {
 
     private final BookCrudService bookCrudService;
 
-    //NEW
     @GetMapping("/api/books")
     public List<BookDto> getAllBooks() {
         return bookCrudService.findAll().stream().map(BookDto::toDto).collect(Collectors.toList());
     }
 
-    //NEW
-    @GetMapping("/api/book/{id}/edit")
-    public BookDto getEditBook(@PathVariable("id") long id) throws NotFoundException {
-        return BookDto.toDto(bookCrudService.findById(id).orElseThrow(NotFoundException::new));
-    }
-
-
-    @PostMapping("/book/delete")
-    public String deleteBook(@RequestParam long id) {
-        bookCrudService.deleteBookById(id);
-        return "redirect:/";
-    }
-
     @PostMapping(value = "/api/book/save")
     public void saveBook(@RequestBody BookDto book) {
         bookCrudService.save(book.toBook());
-    }
-
-    @PostMapping(value = "/book/edit")
-    public String editBook(@ModelAttribute("book") BookDto book) {
-        bookCrudService.updateBookTitleById(book.getId(), book.getTitle());
-        return "redirect:/";
     }
 }
