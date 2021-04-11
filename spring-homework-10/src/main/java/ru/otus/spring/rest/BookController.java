376df1887a7d5143d5f8410598a38c877c11d867
Andrey Smirnov <16.08.9696@mail.ru>
2021-04-11 20:41:27 +0300

    Fix after review

--- spring-homework-10/src/main/java/ru/otus/spring/rest/BookController.java
+++ spring-homework-10/src/main/java/ru/otus/spring/rest/BookController.java
@@ -2,16 +2,14 @@ package ru.otus.spring.rest;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import ru.otus.spring.domain.Book;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import ru.otus.spring.rest.dto.BookDto;
 import ru.otus.spring.service.crud.BookCrudService;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -21,12 +19,12 @@ public class BookController {
 
     private final BookCrudService bookCrudService;
 
-    @GetMapping("/api/books")
+    @GetMapping("/api/book")
     public List<BookDto> getAllBooks() {
         return bookCrudService.findAll().stream().map(BookDto::toDto).collect(Collectors.toList());
     }
 
-    @PostMapping(value = "/api/book/save")
+    @PostMapping("/api/book")
     public void saveBook(@RequestBody BookDto book) {
         bookCrudService.save(book.toBook());
     }
