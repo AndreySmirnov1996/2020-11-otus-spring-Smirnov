47a3f20c510173acc008631de49d7cd35b083996
Andrey Smirnov <andreysm1996@mail.ru>
2021-04-22 23:24:08 +0300

    hw11 refactoring

--- spring-homework-11/src/main/java/ru/otus/spring/page/BookPagesController.java
+++ spring-homework-11/src/main/java/ru/otus/spring/page/BookPagesController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import ru.otus.spring.domain.Book;
 import ru.otus.spring.repositories.BookRepository;
 import ru.otus.spring.rest.dto.BookDto;
@@ -24,12 +21,6 @@ public class BookPagesController {
         return "index";
     }
 
-    @PostMapping("/book/delete")
-    public String deleteBookOld(@RequestParam String id) {
-        bookRepository.deleteById(id);
-        return "redirect:/";
-    }
-
     // Форма для редактирования книги
     @GetMapping("/book/{id}/edit")
     public String editBook(@PathVariable("id") String id, Model model) {
@@ -38,10 +29,14 @@ public class BookPagesController {
         return "edit_book";
     }
 
-//    @PostMapping(value = "/book/edit")
-//    public String editBook(@ModelAttribute("book") BookDto book) {
-//
-//        bookRepository.updateBookTitleById(book.getId(), book.getTitle());
-//        return "redirect:/";
-//    }
+    // TODO пока не работает
+    @PostMapping(value = "/book/edit")
+    public String editBook(@ModelAttribute("book") BookDto bookDto) {
+        Book book = bookRepository.findById(bookDto.getId()).block();
+        if(book != null){
+            book.setTitle(bookDto.getTitle());
+            bookRepository.save(book).block();
+        }
+        return "redirect:/";
+    }
 }
