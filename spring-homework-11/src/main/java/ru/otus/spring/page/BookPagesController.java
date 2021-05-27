8327b4ace18f8a79bd2ca3be3adccdd93a6c64ba
Andrey Smirnov <andreysm1996@mail.ru>
2021-05-27 23:00:16 +0300

    hw11 fix add as reactive new book

--- spring-homework-11/src/main/java/ru/otus/spring/page/BookPagesController.java
+++ spring-homework-11/src/main/java/ru/otus/spring/page/BookPagesController.java
@@ -3,40 +3,15 @@ package ru.otus.spring.page;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import ru.otus.spring.domain.Book;
-import ru.otus.spring.repositories.BookRepository;
-import ru.otus.spring.rest.dto.BookDto;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor
 @Slf4j
 public class BookPagesController {
 
-    private final BookRepository bookRepository;
-
     @GetMapping("/")
     public String indexPage() {
         return "index";
-    }
-
-    // Форма для редактирования книги
-    @GetMapping("/book/{id}/edit")
-    public String editBook(@PathVariable("id") String id, Model model) {
-        Book book = bookRepository.findById(id).block();
-        model.addAttribute("book", BookDto.toDto(book));
-        return "edit_book";
-    }
-
-    // TODO пока не работает
-    @PostMapping(value = "/book/edit")
-    public String editBook(@ModelAttribute("book") BookDto bookDto) {
-        Book book = bookRepository.findById(bookDto.getId()).block();
-        if(book != null){
-            book.setTitle(bookDto.getTitle());
-            bookRepository.save(book).block();
-        }
-        return "redirect:/";
     }
 }
--- spring-homework-11/src/main/resources/static/js/functions.js
+++ spring-homework-11/src/main/resources/static/js/functions.js
@@ -8,16 +8,17 @@ function printAllBooks(books) {
         books.forEach(function (book) {
             $('tbody').append(`
                     <tr>
-                        <td>${book.id}</td>
-                        <td>${book.title}</td>
+                        <td id="${book.id}">${book.id}</td>
+                        <td contenteditable="true">${book.title}</td>
                         <td>${book.genre}</td>
                         <td>${book.authors}</td>
                         <td>
-                            <a href="/book/${book.id}/edit" class="w3-button w3-yellow">Edit</a>
+                            <button type="button" class="w3-button w3-yellow edit-book-btn">EditAjax</button>
                         </td>
                         <td>
                             <input id="deleteBook" type="button" value="Delete" class="w3-button w3-black" onclick="deleteBook('${book.id}')" />
                         </td>
+                        
                     </tr>
                 `)
         });
@@ -40,6 +41,22 @@ function deleteBook(id) {
             getAllBooks();
         },
         error: function (e) {
+        }
+    });
+}
+
+
+function saveBook(book) {
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: "/api/book",
+        data: JSON.stringify(book),
+        success: function () {
+            location.reload();
+        },
+        error: function (error) {
+            alert("Error message:" + error.responseText);
         }
     });
 }
