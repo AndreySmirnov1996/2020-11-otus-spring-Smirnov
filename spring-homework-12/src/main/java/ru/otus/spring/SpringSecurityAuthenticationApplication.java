8f1e84538e758c2aa8c22b9c9f177cba12f87de6
Andrey Smirnov <16.08.9696@mail.ru>
2021-06-07 22:58:43 +0300

    hw12 add main class

--- spring-homework-12/src/main/java/ru/otus/spring/SpringSecurityAuthenticationApplication.java
+++ spring-homework-12/src/main/java/ru/otus/spring/SpringSecurityAuthenticationApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringMVCApplication {
+public class SpringSecurityAuthenticationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringMVCApplication.class, args);
+        SpringApplication.run(SpringSecurityAuthenticationApplication.class, args);
     }
 }
