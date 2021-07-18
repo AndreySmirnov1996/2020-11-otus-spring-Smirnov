0a64280d1d3cc391a68e90e7f3f30857be9c33ca
Andrey Smirnov <andreysm1996@mail.ru>
2021-07-18 15:39:03 +0300

    hw14 fix autoincrement

--- spring-homework-14/src/main/resources/schema.sql
+++ spring-homework-14/src/main/resources/schema.sql
@@ -1,17 +1,16 @@
 drop table if exists authors;
 create table authors
 (
-    id      varchar2(24) IDENTITY primary key,
+    id      varchar2(24) primary key,
     name    varchar2(255),
-    surname varchar2(255),
-    phone   varchar2(20)
+    surname varchar2(255)
 );
 
 
 drop table if exists genres;
 create table genres
 (
-    id   varchar2(24) IDENTITY primary key,
+    id   varchar2(24) primary key,
     name varchar2(255)
 );
 
@@ -19,7 +18,7 @@ create table genres
 drop table if exists books;
 create table books
 (
-    id       varchar2(24) IDENTITY primary key,
+    id       varchar2(24) primary key,
     title    varchar2(255),
     genre_id varchar2(24) references genres (id)
 );
